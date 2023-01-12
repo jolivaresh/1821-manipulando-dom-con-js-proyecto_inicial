6f69d236e79551c8c7b07b7c8c97a877b509b34a
julio Olivares <julio.olivares@gmail.com>
2023-01-12 18:13:24 -0300

    clase04 completada

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+( () => {
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
@@ -8,22 +9,39 @@ const createTask = (evento) => {
     const task = document.createElement("li"); // crea un elemento html
     task.classList.add("card"); // agrega una clase a un elemento HTML
     input.value = "";
-    const content = `<div>  
-         <i class="far fa-check-square icon"></i>
-         <span class="task">${value}</span>
-        </div>
+
+    const taskContent = document.createElement("div");
+    const titletask = document.createElement("samp");
+    titletask.classList.add("task");
+    titletask.innerText = value
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titletask);
+    const content = `
         <i class="fas fa-trash-alt trashIcon icon"></i>`; //contiene el codigo que se debe agragar
-        task.innerHTML = content; // Agrega contenido al HTML
-
-        list.appendChild(task) // agrega un elemento hijo al HTML
-
-        console.log(content);
+        //  ${value} crear un template de un párrafo que reciba el contenido HTML y JavaScript
+        // task.innerHTML = content; // Agrega contenido al HTML
+        task.appendChild(taskContent);
+        list.appendChild(task); // agrega un elemento hijo al HTML
 };
 
-console.log(btn);
 
 // Arrow functions o funciones anonimas
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click", completeTask);
+    return i
+}
+// Inmediately invokerd function expression IIFE 
+const completeTask = (event) => {
+    const element = event.target
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+
+};
 
 /* Utilizar el método 
 addEventListener para escuchar eventos de un elemento
@@ -45,4 +63,4 @@ evento.preventDefault Prevenir el comportamiento patrón del formulario
     const input = document.querySelector("[data-form-input]");
     console.log(input.value);
 }) */
-
+})();
