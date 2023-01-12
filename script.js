46d71ee04f3080f0106a3d1207d0cd0c78b6bdca
julio Olivares <julio.olivares@gmail.com>
2023-01-12 12:38:09 -0300

    clase03 completada

--- script.js
+++ script.js
@@ -3,12 +3,32 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
-}
+    const value = input.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li");
+    task.classList.add("card"); // agrega una clase a un elemento HTML
+    input.value = "";
+    const content = `<div>
+         <i class="far fa-check-square icon"></i>
+         <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+        task.innerHTML = content; // Agrega contenido al HTML
+
+        list.appendChild(task) // agrega un elemento hijo al HTML
+
+        console.log(content);
+};
 
 console.log(btn);
 
+// Arrow functions o funciones anonimas
 btn.addEventListener("click", createTask);
+
+/* Utilizar el método 
+addEventListener para escuchar eventos de un elemento
+evento.preventDefault Prevenir el comportamiento patrón del formulario
+ */
 
 //listener (evento)
 /*btn.addEventListener("click", function (evento) {
